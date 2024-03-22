7a751154a16d47c8e35c051e6ecbc7011e26959e
TaiOdunaiya <tai.odunaiya@yahoo.com>
2024-03-22 02:17:38 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.example.biorhythms',
+  appName: 'Biorhythms',
+  webDir: 'build',
+  bundledWebRuntime:false,
+  
+};
+
+export default config;
